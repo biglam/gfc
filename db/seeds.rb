62e1738e8fdbe8b8d5bfb26bebd3004fb5cc75f7
Craig Lamont <craig@techiteasy.org.uk>
2015-11-16 18:13:18 +0000

    removed reference to match

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.delete_all
-Match.delete_all
 User.create!(email: "admin@admin.example", password: "password", role: "admin", human: true)
 User.create!(email: "user@user.example", password: "password", human: true)
 User.create(email: 'random_computer@example.example', password: 'password', human: false)
