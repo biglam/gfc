9ff92f6921aff8cbff5c241af11eb92086ff8e79
Craig Lamont <craig@techiteasy.org.uk>
2015-11-16 22:24:59 +0000

    added computer players to c4

--- app/controllers/c4games_controller.rb
+++ app/controllers/c4games_controller.rb
@@ -14,13 +14,19 @@ class C4gamesController < ApplicationController
 
       @game.drop(params[:column].to_i, @game.turn.to_s)
       if @game.c4board.check_for_win == nil
-        if @game.turn == 1
-          @game.turn = 2
-        else
+        change_turns
+
+        if @game.turn == 2 && @game.p2.human == false
+          @game.drop(rand(7), "2")
           @game.turn = 1
+          @game.save
         end
         @game.save
+        if @game.c4board.check_for_win == nil
         redirect_to(edit_c4game_path(@game.id))
+      else
+        redirect_to(c4game_path(@game.id))
+      end
       else
         @game.winner_id = @game.c4board.check_for_win
         if @game.winner_id == 1
@@ -59,4 +65,11 @@ class C4gamesController < ApplicationController
     params.require(:c4game).permit(:p1_id, :p2_id)
   end
 
+  def change_turns
+    if @game.turn == 1
+      @game.turn = 2
+    else
+      @game.turn = 1
+    end
+  end
 end
