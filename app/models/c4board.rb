2f270b1b97aaa346f4ad7b7c7e980cc7a8b174a2
Craig Lamont <craig@techiteasy.org.uk>
2015-11-16 16:26:59 +0000

    diagonals working

--- app/models/c4board.rb
+++ app/models/c4board.rb
@@ -2,7 +2,8 @@ class C4board < ActiveRecord::Base
   # belongs_to :c4game
 
   def check_for_win
-    (check_horizontal || check_vertical)
+    # (check_horizontal || check_vertical || check_diagonal)
+    check_diagonal
   end
 
   private
@@ -35,8 +36,40 @@ class C4board < ActiveRecord::Base
   end
 
   def check_diagonal
-    # binding.pry;''
-    #matrix?
-    #search from startpoint
+
+    winner = nil
+    
+    @four = []
+    #downright diagonals
+    3.times do |x|
+      4.times do |y|
+        @four = []
+        @four << send("r#{x+1}").split(//)[y]
+        @four << send("r#{x+2}").split(//)[y+1]
+        @four << send("r#{x+3}").split(//)[y+2]
+        @four << send("r#{x+4}").split(//)[y+3]
+        if @four.join.include? "93xr"
+          winner = 1
+        elsif @four.join.include? "2222"
+          winner = 2
+        end
+      end
+    end
+    #downleft diagonals
+    3.times do |x|
+      4.times do |y|
+        @four = []
+        @four << send("r#{x+1}").split(//)[6-y]
+        @four << send("r#{x+2}").split(//)[5-y]
+        @four << send("r#{x+3}").split(//)[4-y]
+        @four << send("r#{x+4}").split(//)[3-y]
+        if @four.join.include? "1111"
+          winner = 1
+        elsif @four.join.include? "2222"
+          winner = 2
+        end
+      end
+    end
+    return winner
   end
 end
